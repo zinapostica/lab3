2cbdf376fe6957a79003bcf3627861cb0f95a98e
zinapostica <56154980+zinapostica@users.noreply.github.com>
2020-10-22 22:12:39 +0300

    Update server.php

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// initializing variables
+
 $username = "";
 $email    = "";
 $errors = array();
@@ -20,8 +20,7 @@ if (isset($_POST['reg_user'])) {
     $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
     $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-    // form validation: ensure that the form is correctly filled ...
-    // by adding (array_push()) corresponding error unto $errors array
+
     if (empty($username)) {
         array_push($errors, "Username is required");
     }
@@ -35,8 +34,6 @@ if (isset($_POST['reg_user'])) {
         array_push($errors, "The two passwords do not match");
     }
 
-    // first check the database to make sure 
-    // a user does not already exist with the same username and/or email
     $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
@@ -51,9 +48,9 @@ if (isset($_POST['reg_user'])) {
         }
     }
 
-    // Finally, register user if there are no errors in the form
+
     if (count($errors) == 0) {
-        $password = md5($password_1); //encrypt the password before saving in the database
+        $password = md5($password_1);
 
         $query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
